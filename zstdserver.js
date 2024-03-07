5e23d6e88f231d6be404ca6c024844ae8aa5bc3d
samuellorentz <samuel.lorentz@trustly.com>
2024-03-07 17:35:05 -0300

    Enpoint that returns content as zstd

---
+++ zstdserver.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const fs = require('fs');
+const  zstd = require('@mongodb-js/zstd');
+
+const app = express();
+const PORT = 3000;
+
+app.get('/zstd', async (req, res) => {
+  try {
+    const data = fs.readFileSync('content.html');
+    const compressedData = await zstd.compress(data, 5);
+
+    res.setHeader('Content-Encoding', 'zstd');
+    res.setHeader('Content-Type', 'text/plain');
+    res.send(compressedData);
+  } catch (err) {
+    console.error(err);
+    res.status(500).send('Erro ao ler ou comprimir o arquivo');
+  }
+});
+
+app.listen(PORT, () => {
+  console.log(`Servidor rodando na porta ${PORT}`);
+});
