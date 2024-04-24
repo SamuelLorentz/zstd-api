07cecab96d3aab1c9f2a2c0bd5ebff37ca1b7678
samuellorentz <samuel.lorentz@trustly.com>
2024-04-24 09:34:08 -0300

    Adding header log handler

--- index.js
+++ index.js
@@ -8,6 +8,30 @@ const PORT = 3000;
 
 app.get("/", (req, res) => res.send("Express on Vercel"));
 
+app.get("/", (req, res) => {
+
+  try {
+    
+    console.log(req.headers);
+
+    let headersHtml = '<html><head><title>Headers</title></head><body><h1>Request Headers</h1><ul>'; 
+    for (let header in req.headers) {
+        headersHtml += `<li><strong>${header}:</strong> ${req.headers[header]}</li>`;
+    }    
+    headersHtml += '</ul></body></html>';
+
+    res.setHeader('Content-Encoding', 'zstd');
+    res.setHeader('Content-Type', 'text/html');
+    
+    const compressedData = zstd.compress(headersHtml, 3);
+    res.send(compressedData);
+
+  } catch (err) {
+    console.error(err);
+    res.status(500).send('Erro ao ler ou comprimir o arquivo');
+  }
+});
+
 app.get('/zstd', async (req, res) => {
   try {
     let contentPath = path.join(process.cwd(), 'content.html');
